627e9a406ec02c832907d07a918a7f5207dca68f
Ben <benbruscella@gmail.com>
2011-02-13 22:19:51 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bankio::Application.config.secret_token = 'c62c4f4d96d33073d2f7b266324a743a4be7e210cf749708466fbbd7a76c5f59d5aa53bfbf39e9375e42b83b388b39a5e7296505c387e5dfc62bff46ac7b1b0b'
